66a2f4667407a23ceffdcec02492cb0b430bf6db
Leh6201 <leticiacorain57@gmail.com>
2023-05-09 16:15:40 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -21,23 +21,32 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
+	
+
+	
 
 	public Game() {
 
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -76,11 +85,20 @@ public class Game {
 	}
 
 	public String getPlatform() {
-		return platform;
+		return platforms;
+	}
+	
+	public void setPlatform(String platform) {
+		this.platforms = platform;
+	}
+	
+
+	public Double getScore() {
+		return score;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
